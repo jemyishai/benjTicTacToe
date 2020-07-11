28a4665a7db8d19f1fb03eb2e973786eeab7eb55
Jemyishai <jesse.moskowitz@gmail.com>
2020-07-11 02:12:06 -0400

    initial scaffolding

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Benj-a-boog-a-loo's Tic Tac Toe </div>
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a tic tac toe board for my nephew Benj. It's also an opporunity for me to learn/ review React Hooks. 
